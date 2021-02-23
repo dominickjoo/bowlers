4cb12b77595470c08a2eb6ca5cc1f014e2c94fb6
Dominick Joo <dominickijoo@gmail.com>
2021-02-23 07:45:14 -0500

    typo in authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     
     app.get('/auth/google/callback', passport.authenticate('google'));
 
-    app.get('api/logout', (req, res) => {
+    app.get('/api/logout', (req, res) => {
        req.logout();
        res.send(req.user); 
     });
