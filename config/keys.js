0a78ca9c9ad478790b778fe3eb19324637e57f58
Dominick Joo <dominickijoo@gmail.com>
2021-02-23 07:41:08 -0500

    google strategy proxy = true

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
-// mongodb+srv://dominick_joo:ONerzRrb0Vt6B7FU@cluster0.ies8d.mongodb.net/database?retryWrites=true&w=majority
-
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else { // development
