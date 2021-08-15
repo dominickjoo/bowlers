e5b5d81f1cba6b8535b464bdc1d23b6b4d2a2ac4
Dominick Joo <dominickijoo@gmail.com>
2021-08-15 17:14:58 -0400

    add fetchUser to authReducer

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,16 +1,20 @@
-function Header() {
-  return (
-    <nav>
-      <div className="nav-wrapper">
-        <a className="left brand-logo">
-          Bowlers
-        </a>
-        <ul className="right">
-          <a>Login with Google</a>
-        </ul>
-      </div>
-    </nav>
-  );
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+            Bowlers
+          </a>
+          <ul className="right">
+            <a>Login with Google</a>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Header;
