fe850065163dda8db94c1a61b5ebaa4832cc381f
Dominick Joo <dominickijoo@gmail.com>
2021-08-15 18:19:44 -0400

    finish google oauth login logout flow

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,7 +21,7 @@ class Header extends Component {
             Bowlers
           </a>
           <ul className="right">
-            <a>Login with Google</a>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
