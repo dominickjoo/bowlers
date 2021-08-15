ebb66dc65d1c5bae29c5c68d6570ed013d8dd3f8
Dominick Joo <dominickijoo@gmail.com>
2021-08-15 14:53:10 -0400

    some frontend stuff??

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,8 +1,15 @@
 function Header() {
   return (
-    <div>
-      Header Component
-    </div>
+    <nav>
+      <div className="nav-wrapper">
+        <a className="left brand-logo">
+          Bowlers
+        </a>
+        <ul className="right">
+          <a>Login with Google</a>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
