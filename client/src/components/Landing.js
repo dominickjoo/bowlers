73dd02bfd6752dbfbf428c1a7687c943f3b4e826
Dominick Joo <dominickijoo@gmail.com>
2021-08-16 14:28:53 -0400

    make logo go back to home page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Bowlers
+      </h1>
+      Science Bowl Yay
+    </div>
+  );
+};
+
+export default Landing;
